df35586ec5f6136ffe8aa31bf02b7b29cad9cafb
Matthew Kory <matthewskory@gmail.com>
2016-07-11 13:42:01 -0400

    updated swanson to https

--- swanson/main.js
+++ swanson/main.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
 	$("#getquote").click(function(){
 		$.ajax({
 		  type: 'GET',
-		  url: 'http://ron-swanson-quotes.herokuapp.com/v2/quotes',
+		  url: 'https://ron-swanson-quotes.herokuapp.com/v2/quotes',
 		  success: function(data) {
 		    $("#quotes").text(data[0]); 
 		  }
